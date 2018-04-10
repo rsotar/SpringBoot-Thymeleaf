101c42a934a9b3d17304ee8fc1525fa8dd344c3b
roberto.sotar <roberto.sotar@globant.com>
2018-04-09 22:38:09 -0300

    Add fix for CSS issues and SSL exception for DB url

--- src/main/java/com/rsotar/backendninja/configuration/SecurityConfiguration.java
+++ src/main/java/com/rsotar/backendninja/configuration/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-			.antMatchers("*/static/*").permitAll()
+			.antMatchers("/css/**","/imgs/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin().loginPage("/login").loginProcessingUrl("/logincheck")
